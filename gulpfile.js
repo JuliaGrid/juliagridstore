55f7fe1d71b1635e8e25e6d0df0603b2437f7d42
JuliaGrid <julia.grid2000@gmail.com>
2019-10-03 11:05:09 +0300

    Первичная стилизация главной страницы, баг с gulp файлом, не обновляется автоматически страница

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,35 @@
-'use strict';
+"use strict";
 
-var gulp = require('gulp');
-var less = require('gulp-less');
-var plumber = require('gulp-plumber');
-var sourcemap = require('gulp-sourcemaps');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var server = require('browser-sync').create();
+var gulp = require("gulp");
+var less = require("gulp-less");
+var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
 
-gulp.task('css', function () {
-    return gulp.src('source/less/style.less')
+gulp.task("css", function () {
+    return gulp.src("source/less/style.less")
         .pipe(plumber())
         .pipe(sourcemap.init())
         .pipe(less())
         .pipe(postcss([
             autoprefixer()
         ]))
-        .pipe(sourcemap.write('.'))
-        .pipe(gulp.dest('source/css'));
+        .pipe(sourcemap.write("."))
+        .pipe(gulp.dest("source/css"));
 });
 
-gulp.task('server', function () {
+gulp.task("server", function () {
     server.init({
-        server: 'source/'
+        server: "source/",
+        notify: false,
+        open: true,
+        cors: true,
+        ui: false
     });
-    gulp.watch('source/less/**/*.less', gulp.series('css'));
-    gulp.watch('source/*.html').on('change', server.reload);
+    gulp.watch("source/less/**/*.less", gulp.series("css"));
+    gulp.watch("source/*.html").on("change", server.reload);
 });
 
-gulp.task('start', gulp.series('css', 'server'));
+gulp.task("start", gulp.series("css", "server"));
